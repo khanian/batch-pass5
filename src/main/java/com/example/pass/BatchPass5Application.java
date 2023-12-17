3b8ba6521bfae13bf0ddf33a5472e62318c0e3b2
khanian <khanylee@gmail.com>
2023-12-17 11:14:11 +0900

    Replace with lambda 로 변경하여 warning 줄이기.

--- src/main/java/com/example/pass/BatchPass5Application.java
+++ src/main/java/com/example/pass/BatchPass5Application.java
@@ -2,13 +2,10 @@ package com.example.pass;
 
 import org.springframework.batch.core.Job;
 import org.springframework.batch.core.Step;
-import org.springframework.batch.core.StepContribution;
 import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.batch.core.job.builder.JobBuilder;
 import org.springframework.batch.core.repository.JobRepository;
-import org.springframework.batch.core.scope.context.ChunkContext;
 import org.springframework.batch.core.step.builder.StepBuilder;
-import org.springframework.batch.core.step.tasklet.Tasklet;
 import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -29,12 +26,9 @@ public class BatchPass5Application {
     @Bean
     public Step passStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
         return new StepBuilder("passStep", jobRepository)
-                .tasklet(new Tasklet() {
-                    @Override
-                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
-                        System.out.println("Excution PassStep with Spring batch 5!!");
-                        return RepeatStatus.FINISHED;
-                    }
+                .tasklet((contribution, chunkContext) -> {
+                    System.out.println("Excution PassStep with Spring batch 5!!");
+                    return RepeatStatus.FINISHED;
                 }, transactionManager).build();
     }
 
