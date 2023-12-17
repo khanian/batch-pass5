7b040f97c1fdd9a99f5ddfdb6059dc052e830a6f
khanian <khanylee@gmail.com>
2023-12-17 10:55:26 +0900

    Initial commit

---
+++ src/main/java/com/example/pass/BatchPass5Application.java
@@ -0,0 +1,13 @@
+package com.example.pass;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchPass5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BatchPass5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/pass/BatchPass5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.pass;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchPass5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
