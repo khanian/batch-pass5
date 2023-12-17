9613b1150f88fe00afc24fe7be319632608112b0
khanian <khanylee@gmail.com>
2023-12-17 11:11:37 +0900

    EnableBatchProcessing 을 안 붙여도 실행은 된다. 붙이니까 Warning이 줄어들었다.

--- src/main/java/com/example/pass/BatchPass5Application.java
+++ src/main/java/com/example/pass/BatchPass5Application.java
@@ -1,10 +1,42 @@
 package com.example.pass;
 
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.job.builder.JobBuilder;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.builder.StepBuilder;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.transaction.PlatformTransactionManager;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class BatchPass5Application {
+
+    @Bean
+    public Job passJob(JobRepository jobRepository, Step passStep) {
+        return new JobBuilder("passJob", jobRepository)
+                .start(passStep)
+                .build();
+    }
+
+    @Bean
+    public Step passStep(JobRepository jobRepository, PlatformTransactionManager transactionManager) {
+        return new StepBuilder("passStep", jobRepository)
+                .tasklet(new Tasklet() {
+                    @Override
+                    public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                        System.out.println("Excution PassStep with Spring batch 5!!");
+                        return RepeatStatus.FINISHED;
+                    }
+                }, transactionManager).build();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(BatchPass5Application.class, args);
